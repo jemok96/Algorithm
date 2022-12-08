d319dd662c1290bc3d73a8a306c95bb6c8c080a3
경제목 <jemok9605@naver.com>
2022-12-08 17:54:40 +0900

    Create 유클리드호제법.py

---
+++ Baeckjoon/유클리드호제법.py
@@ -0,0 +1,20 @@
+a, b = 120, 48
+# 최대공약수
+
+
+def gcd(a, b):
+    while b > 0:
+        a, b = b, a % b
+    return a
+
+
+print(a)
+
+# 최소공배수
+
+
+def lcm(a, b):
+    return int(a*b/gcd(a, b))
+
+
+print(lcm(a, b))
