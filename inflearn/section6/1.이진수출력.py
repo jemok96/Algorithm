d7f3bc279a8be067604075106f1dbcb09b20abf5
경제목 <jemok9605@naver.com>
2023-02-27 14:17:58 +0900

    inflearn section6
    inflearn section6  DFS 연습문제

---
+++ inflearn/section6/1.이진수출력.py
@@ -0,0 +1,14 @@
+# 10진수 N이 입력되면 2진수로 변환하여 출력하는 프로그램을 작성하세요. 단 재귀함수를 이용
+# 해서 출력해야 합니다.
+n = int(input())
+
+
+def DFS(x):
+    if x == 0:
+        return
+    else:
+        DFS(x//2)
+        print(x % 2, end="")
+
+
+DFS(n)
